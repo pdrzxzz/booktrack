0b4f1509b5ee2dbe874dde2c1ea162368712bd4b
Emanuel Pedroza <emanuelsalgado007@gmail.com>
2025-05-09 21:18:32 -0300

    initial commit, create basic layer structure, import and use express.

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+// start server
+const app = express();
+//path to use __dirname and search views dir from app.js exec
+const path = require('path'); 
+
+// app.set('views', path.join(__dirname, 'views'))
+
+const indexRoutes = require('./routes/index')
+app.use('/', indexRoutes)
+
+//if any other route do not match, throw error
+// app.all('*', (req, res, next) => {
+//     next(new ExpressError('Page Not Found', 404))
+// })
+
+//start listening
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
+})
